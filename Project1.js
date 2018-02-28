d174b0e685eccf85b54e985e70d7e3c656de349a
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2018-02-27 19:17:03 -0800

    started to work on a function 'displayBoard'

--- Project1.js
+++ Project1.js
@@ -69,9 +69,9 @@ return newBoard;
 }
 console.log(listBoardsNew(hello));
 
-// Function that removes board
+//Function that removes board
 
-let boardName = 'Dreams';
+//let boardName = 'Dreams';
 
 function removeBoard(boardName) {
 let boardList = Object.getOwnPropertyNames(hello);
@@ -79,21 +79,51 @@ let boardList = Object.getOwnPropertyNames(hello);
   if (boardList.includes(boardName)){ //Object.keys(hello))
     delete hello[boardName]; //delete hello[key];
   console.log(`Board "${boardName}" was deleted!`);
+
   } else {
   console.log('Board doesn\'t exist');
   }
 return boardName;
   }
-
-
-removeBoard(boardName);
+//
+//
+// removeBoard(boardName);
 //////////////////////////////////
 //console.log(hello.dreams);
 //console.log(Object.keys(hello));
 //////////////////////////////////
 
 // Display Board function
+let boardName = 'Dreams';
 
 function displayBoard(boardName) {
+  let boardList = Object.getOwnPropertyNames(hello);
+  let list = Object.entries(hello[boardName]);
+    if (boardList.includes(boardName)){
+      // console.log(hello[boardName]);
+      for (let board in hello[boardName]) {
+        console.log(`${board}>>>>`);
+        for(let i=0; i<hello[boardName][board].length; i++) {
 
+          console.log(hello[boardName][board][i]);
+        }
+
+        // hello[boardName][board].map((i) => {
+        // })
+      }
+    } else {
+    console.log('Board doesn\'t exist');
+    }
+  return boardName;
 }
+
+displayBoard(boardName);
+
+// ///////////////////////////////////////////
+// let newBoard = "----------------------\n";
+// let count = 0;
+// for (let o in obj) {
+//   count++;
+//   newBoard += `${count} - ${o}\n----------------------\n`;
+//
+//   //////////////////////////////////////////////
